54787cc6df3e1ff6a9667e63a31b14a8caa4d435
ELIZA <rysaeva96@mail.ru>
2022-05-30 22:51:11 +0500

    Решение 39 задачи

---
+++ Task_39/Program.cs
@@ -0,0 +1,26 @@
+﻿// Написать программу, которая перевернет
+// одномерный массив (последний элемент будет на первом
+// месте, а первый - на последнем и т.д.)
+// [ 1 2 3 4 5]
+
+Console.Write("Введите элементы массива через пробел: ");
+int[] array = Console.ReadLine().Split().Select(int.Parse).ToArray();
+// Split - делит сроку (по пробелу или букве)
+// Select(int.Parse) - конвентация в int из string
+// ToArray - приводим к типу массива (т.к. select возвращает коллекцию)
+
+Console.WriteLine(String.Join(" ", array));
+int size = array.Length;
+int index1 = 0;
+int index2 = size - 1;
+
+while (index1 < index2)
+{
+    int temp = array[index1];
+    array[index1] = array[index2];
+    array[index2] = temp;
+
+    index1++;
+    index2--;
+}
+Console.WriteLine(String.Join(" ", array));
