7842e1c1df71de86df32da441cc17f5c5697c1a2
ELIZA <rysaeva96@mail.ru>
2022-06-03 00:39:18 +0500

    Решение 45 задачи с поэлементным копированием

---
+++ Task_45/Program.cs
@@ -0,0 +1,37 @@
+﻿// Напишите программу, которая будет создавать копию 
+// заданного массива с помощью поэлементного копирования
+
+int[] GenArray()
+{
+    Console.Write("Введите число элементов массива: ");
+    int n = int.Parse(Console.ReadLine());
+    int[] arr = new int[n];
+    Random rand = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rand.Next(11);
+    }
+    return arr;
+}
+
+void PrintArray(int[] prarr)
+{
+    var str = string.Join(" ", prarr);
+    Console.WriteLine(str);
+}
+
+int[] CopyArr(int[] arr)
+{
+    int[] coparr = new int[arr.Length];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        coparr[i] = arr[i];
+    }
+    return coparr;
+}
+int[] arr = GenArray();
+PrintArray(arr);
+int[] newarr = CopyArr(arr);
+PrintArray(newarr);
+
+
