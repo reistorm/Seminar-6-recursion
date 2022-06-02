f36225cad5e8c1c8dd07e5a8b86f90b482128971
ELIZA <rysaeva96@mail.ru>
2022-06-02 18:43:04 +0500

    Решение 40 задачи

---
+++ Task_42/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая будет преобразовывать
+// десятичное число в двоичное
+// 45 - 101101    3- 11    2- 10
+
+Console.Write("Введите число: ");
+int decnum = int.Parse(Console.ReadLine());
+
+void BinNum(int num)
+{
+    string binnum = "";
+    while (num > 0)
+    {
+        string text = (num % 2).ToString();
+        num = num / 2;
+        binnum = binnum + text;
+    }
+    char[] arr = binnum.ToCharArray(); // char - символ, разбиваем строку поэлементов - массив символов
+
+    Array.Reverse(arr);
+    var str = string.Join(" ", arr);
+    Console.WriteLine($"Число {decnum} в двоичном формате: {str}"); 
+}
+
+BinNum(decnum);
