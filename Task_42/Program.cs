7d60916c7cf9c2fdb3fe9afb8ee2631a29c38153
ELIZA <rysaeva96@mail.ru>
2022-06-02 22:57:55 +0500

    Решение задачи 42

--- Task_42/Program.cs
+++ Task_42/Program.cs
@@ -24,20 +24,58 @@
 // BinNum(decnum);
 
 // чтобы не разворачивать массив
-Console.Write("Введите число: ");
-int decnum = int.Parse(Console.ReadLine());
+// Console.Write("Введите число: ");
+// int decnum = int.Parse(Console.ReadLine());
 
-void BinNum(int num)
+// void BinNum(int num)
+// {
+//     string binnum = "";
+//     while (num > 0)
+//     {
+//         string text = (num % 2).ToString();
+//         num = num / 2;
+//         binnum = text + binnum; // text пишем перед binnum чтобы развернуть сразу
+//     }
+
+//     Console.WriteLine(binnum);
+// }
+
+// BinNum(decnum);
+
+
+// вариант преподавателя
+
+Console.WriteLine("Введите десятичное число: ");
+int numDec = int.Parse(Console.ReadLine());
+Console.Write("Введите основание системы счисления: ");
+int baseNum = int.Parse(Console.ReadLine());
+
+int result = 0;
+int d10 = 1;
+while (numDec != 0)
 {
-    string binnum = "";
-    while (num > 0)
-    {
-        string text = (num % 2).ToString();
-        num = num / 2;
-        binnum = text + binnum; // text пишем перед binnum чтобы развернуть сразу
-    }
-   
-    Console.WriteLine(binnum); 
+    result = result + numDec % baseNum * d10;
+    numDec /= 2; // numDec = numDec/=2 сокращенный вариант
+    d10 = d10 * 10;
 }
+Console.WriteLine(result);
 
-BinNum(decnum);
+
+
+
+
+// для любой системы счисления через рекурсию
+Console.WriteLine("Введите десятичное число: ");
+int numDec = int.Parse(Console.ReadLine());
+Console.Write("Введите основание системы счисления: ");
+int baseNum = int.Parse(Console.ReadLine()); 
+void DecToOther(int num, int baseN)
+{
+    if (num == 0)
+    {
+        return;
+    }
+    DecToOther(num / baseN, baseN);
+    Console.Write(num % baseN);
+}
+DecToOther(numDec, baseNum);
